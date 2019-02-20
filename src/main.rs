d3cfae11cde440f7d48d16752da27b5cf83a70c5
Jason Harris <sirmanlypowers@gmail.com>
2019-02-20 07:02:12 -0800

    WIP.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+extern crate cortex_m_rt;
+extern crate panic_halt;
+extern crate stm32f4;
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use stm32f4::stm32f407;
+//use cortex_m::asm;
+
+//#define IO_LED_GREEN      GPIO_NUM(PORTD, 12)	// GPIO: green led
+//#define IO_LED_AMBER      GPIO_NUM(PORTD, 13)	// GPIO: amber led
+//#define IO_LED_RED        GPIO_NUM(PORTD, 14)	// GPIO: red led
+//#define IO_LED_BLUE       GPIO_NUM(PORTD, 15)	// GPIO: blue led
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    // get handles to the hardware
+    let peripherals = stm32f407::Peripherals::take().unwrap();
+    let gpiod = &peripherals.GPIOD;
+    let rcc = &peripherals.RCC;
+
+    // enable the GPIO clock for IO port D
+    rcc.ahb1enr.write(|w| w.gpioden().set_bit());
+
+    //gpiod.crh.write(|w| unsafe{
+    //    w.mode13().bits(0b11);
+    //    w.cnf13().bits(0b00)
+    //});
 
     loop {
-        panic!("Oops")
+        gpiod.bsrr.write(|w| w.bs12().set_bit());
+        cortex_m::asm::delay(2000000);
+        gpiod.brr.write(|w| w.br12().set_bit());
+        cortex_m::asm::delay(2000000);
     }
 }
